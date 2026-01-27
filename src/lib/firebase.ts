729832bf7ee6636ba22d53d953970566808f2424
JULAME <bernalgarciaaldoelias@gmail.com>
2026-01-27 16:56:05 -0600

    Update Firebase configuration with hardcoded values
    Replaced environment variables with hardcoded Firebase configuration values.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID!,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyCGp8e6ldrkYyN1klcNx1KfNm2r0ODFkcQ",
+  authDomain: "trianglerh-63d38.firebaseapp.com",
+  projectId: "trianglerh-63d38",
+  appId: "1:1077110947594:web:4c66959a7b77adf55f04f3",
+  messagingSenderId: "1077110947594",
 };
+
 
 function getFirebaseApp() {
   if (getApps().length) return getApp();
